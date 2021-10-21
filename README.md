10a95739c21d57d32fae7f887562da098698ad41
Martyx00 <martin.petran@pm.me>
2021-10-21 07:48:44 +0200

    v1.3

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-# CollaRE v1.2
+# CollaRE v1.3
 
 ![CollaRE](./collare/icons/collare-full-white.png)
 
--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from glob import glob
 
 setup(
     name='collare',
-    version='1.2',
+    version='1.3',
     author='Martin Petran',
     license='Apache 2.0',
     description='Multi-tool collaboration for reverse engineers.',
     url='https://github.com/Martyx00/CollaRE',
-    download_url='https://github.com/Martyx00/CollaRE/archive/refs/tags/v1.2.tar.gz',
+    download_url='https://github.com/Martyx00/CollaRE/archive/refs/tags/v1.3.tar.gz',
     packages = ['collare'],
     package_data = {'collare' : ["icons/*"] },
     entry_points={
